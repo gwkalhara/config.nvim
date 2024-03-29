00411ece991ea1c61182f2ab57a031bb2c8a4f8e
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-03-29 20:37:57 +0530

    updated plugin
    defined env folder names for python-venv-selector

--- lua/custom/plugins/lang/python/venv-selector.lua
+++ lua/custom/plugins/lang/python/venv-selector.lua
@@ -3,8 +3,12 @@ return {
 	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
 	opts = {
 		-- Your options go here
-		-- name = "venv",
-		-- auto_refresh = false
+    name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+    }
 	},
 	ft = "python",
 	keys = {

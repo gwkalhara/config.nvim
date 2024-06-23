35851cf8915a16a7c727ba63cf3626fb1acbb9cf
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-06-23 10:17:37 +0530

    added miniconda support

--- lua/custom/plugins/lang/python.lua
+++ lua/custom/plugins/lang/python.lua
@@ -1,15 +1,6 @@
 return {
 	"linux-cultist/venv-selector.nvim",
 	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {
-		-- Your options go here
-    name = {
-        "venv",
-        ".venv",
-        "env",
-        ".env",
-    }
-	},
   --#NOTE: used for the new version(2024)
   branch = "regexp", -- This is the regexp branch, use this for the new version
 	ft = "python",
@@ -21,7 +12,15 @@ return {
 		{ "<leader>cvp", function () print(require("venv-selector").venv()) end},
 	},
 	config = function()
-		require("venv-selector").setup({})
+		require("venv-selector").setup({
+      settings = {
+        search = {
+          miniconda3 = {
+            command = "fd python.exe$ C:\\Users\\Hp\\miniconda3\\envs\\"
+          }
+        }
+      }
+    })
 		local wk = require("which-key")
 		wk.register({
 			v = {

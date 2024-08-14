0f3e3f019ed01bac35dd87a0cbdb4f1d451c06e5
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-08-14 09:58:38 +0530

    update telescope keymap

--- lua/custom/plugins/util/telescope.lua
+++ lua/custom/plugins/util/telescope.lua
@@ -4,23 +4,15 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("telescope").setup()
-		-- local builtin = require('telescope.builtin')
-		--vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files"})
-		--vim.keymap.set('n', '<leader>fs', builtin.live_grep, {desc = "Search string"})
-		--vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Search buffers"})
-		--vim.keymap.set('n', '<leader>fg', builtin.git_files, {desc = "Find Git files"})
-		--vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {desc = "Find recent files"})
 
 		local wk = require("which-key")
-		wk.register({
-			f = {
-				name = "find",
-				f = { "<cmd>Telescope find_files<cr>", "Find files" },
-				s = { "<cmd>Telescope live_grep<cr>", "Search string" },
-				b = { "<cmd>Telescope buffers<cr>", "Search buffers" },
-				g = { "<cmd>Telescope git_files<cr>", "Find Git files" },
-				r = { "<cmd>Telescope oldfiles<cr>", "Find recent files" },
-			},
-		}, { prefix = "<leader>" })
+    wk.add({
+      { "<leader>f", group = "find"},
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Search string" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Search buffers" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git files" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Find recent files" },
+    })
 	end,
 }

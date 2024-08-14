ecd853d5d01e545b5b3db5143e2c68ac448ec9b2
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-08-14 10:17:03 +0530

    remove colorizer keymap

--- lua/custom/plugins/ui/colorizer.lua
+++ lua/custom/plugins/ui/colorizer.lua
@@ -4,12 +4,5 @@ return {
   config = function ()
     local colorizer = require("colorizer")
     colorizer.setup()
-    local wk = require("which-key")
-    wk.register({
-      U = {
-        name = "UI",
-        c = { "<cmd>ColorizerToggle<cr>", "Toggle colorizer"}
-      }
-    }, {prefix = "leader"})
   end
 }

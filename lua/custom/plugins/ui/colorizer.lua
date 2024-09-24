8348555260e13df0335b16fff0fe25ed70cf6eb9
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-09-24 11:49:25 +0530

    added colorizer toggle mapping

--- lua/custom/plugins/ui/colorizer.lua
+++ lua/custom/plugins/ui/colorizer.lua
@@ -4,5 +4,9 @@ return {
   config = function ()
     local colorizer = require("colorizer")
     colorizer.setup()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>Uc", "<cmd>ColorizerToggle<cr>", desc = "Toggle colorizer" }
+    })
   end
 }

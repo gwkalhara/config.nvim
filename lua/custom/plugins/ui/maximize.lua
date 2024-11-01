1f0fbf1156c950805cd6c796d141b8aae9963b75
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2024-11-01 22:13:22 +0530

    .

--- lua/custom/plugins/ui/maximize.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "declancm/maximize.nvim",
-  event = "BufWinEnter",
-  config = function ()
-    require("maximize").setup()
-    vim.keymap.set("n", "<leader>bM", function() require("maximize").toggle() end, {desc = "Toggle maximized window"})
-  end,
-}

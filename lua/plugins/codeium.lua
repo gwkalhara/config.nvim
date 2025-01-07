726b5213e60250743f656888d321cc2344b2f524
gamithaKalharaW <136357643+gamithaKalharaW@users.noreply.github.com>
2025-01-07 14:44:36 +0530

    chore: re-enabled with slightly changed config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,16 +2,40 @@ return {
   -- INFO: Original repo
   -- "monkoose/neocodeium",
   "gamithaKalharaW/neocodeium.clone",
-  lazy = true,
+  event = "VeryLazy",
+  -- lazy = true,
   config = function()
-    local neocodeium = require "neocodeium"
-    -- neocodeium.setup({ enabled = false })
-    neocodeium.setup()
+    local neocodeium = require("neocodeium")
+    neocodeium.setup({
+      enabled = true,
+      manual = true,
+    })
+    -- neocodeium.setup()
 
-    vim.keymap.set("i", "<c-]>", neocodeium.accept, { desc = "Accept codeium prompt" })
-    vim.keymap.set("i", "<A-w>", neocodeium.accept_word, { desc = "Accept codeium word" })
-    vim.keymap.set("i", "<A-a>", neocodeium.accept_line, { desc = "Accept codeium line" })
-    vim.keymap.set("i", "<A-e>", neocodeium.cycle_or_complete, { desc = "Cycle prompt up" })
+    vim.keymap.set(
+      "i",
+      "<c-]>",
+      neocodeium.accept,
+      { desc = "Accept codeium prompt" }
+    )
+    vim.keymap.set(
+      "i",
+      "<A-w>",
+      neocodeium.accept_word,
+      { desc = "Accept codeium word" }
+    )
+    vim.keymap.set(
+      "i",
+      "<A-a>",
+      neocodeium.accept_line,
+      { desc = "Accept codeium line" }
+    )
+    vim.keymap.set(
+      "i",
+      "<A-e>",
+      neocodeium.cycle_or_complete,
+      { desc = "Cycle prompt up" }
+    )
     vim.keymap.set("i", "<A-r>", function()
       neocodeium.cycle_or_complete(-1)
     end, { desc = "Cycle prompt down" })

980ae31a08d3d7a33635e6e00e3f3a5925d899bf
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2025-04-16 21:28:47 +0530

    fix: Remove old codeium spec

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,3 @@
--- return {
---   "github/copilot.vim",
---   config = function()
---     vim.keymap.set("i", "<C-\\>", 'copilot#Accept("\\<CR>")', {
---       expr = true,
---       replace_keycodes = false,
---     })
---     vim.g.copilot_no_tab_map = true
---   end,
--- }
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
@@ -27,9 +17,3 @@ return {
     },
   },
 }
-
--- vim.keymap.set("i", "<c-]>", neocodeium.accept, { desc = "Accept codeium prompt" })
--- vim.keymap.set("i", "<A-w>", neocodeium.accept_word, { desc = "Accept codeium word" })
--- vim.keymap.set("i", "<A-a>", neocodeium.accept_line, { desc = "Accept codeium line" })
--- vim.keymap.set("i", "<A-e>", neocodeium.cycle_or_complete, { desc = "Cycle prompt up" })
--- vim.keymap.set("i", "<A-r>", function()

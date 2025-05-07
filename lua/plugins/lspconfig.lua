89d5c54aa11d40c0169932c58a9ae9d44b4b3440
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2025-05-07 07:02:20 +0530

    feat!: update mason specs
    feat!: fix mason-tool-installer to new spec

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -17,13 +17,15 @@ return {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       {
         -- NOTE: Mason must be loaded before dependants
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
+        version = "*",
         opts = {
           max_concurrent_installers = 2,
         },
       },
       {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
+        version = "*",
         dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
       },
       {
@@ -266,18 +268,7 @@ return {
       })
 
       ---@diagnostic disable-next-line: missing-fields
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
-      })
+      require("mason-lspconfig").setup({})
     end,
   },
 }

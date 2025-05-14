8c1e319e06f7b829711d457d77d9746f2b08b361
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2025-05-14 21:10:53 +0530

    fix: gopls server setup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -296,7 +296,26 @@ return {
       }
       vim.lsp.enable("lua_ls")
 
-      vim.lsp.config.gopls = gopls_config
+      vim.lsp.config.gopls = {
+        cmd = { "gopls" },
+        filetypes = { "go", "gotempl", "gowork", "gomod" },
+        root_markers = { ".git", "go.mod", "go.work", vim.uv.cwd() },
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+            ["ui.inlayhint.hints"] = {
+              compositeLiteralFields = true,
+              constantValues = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+          },
+        },
+      }
       vim.lsp.enable("gopls")
 
       vim.lsp.config.basedpyright = {

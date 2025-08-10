5d64a67bf5d929be8717876d0dfed7e01e2872fd
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2025-08-10 18:40:38 +0530

    fix: disable lazy update checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true, concurrency = 4, notify = false, frequancy = 43200 },
+  checker = { enabled = false, concurrency = 4, notify = false, frequancy = 43200 },
   rocks = { enabled = false },
   change_detection = {
     enabled = true,

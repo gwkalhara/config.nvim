b127ea55ca104d4d3e9caf4cd067996cddb5340b
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2025-10-07 17:41:29 +0530

    chore(spec): remove duplicate line

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,7 +9,6 @@ return {
     vim.g.vimtex_view_general_viewer = "SumatraPDF"
     vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 
-    vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
     vim.g.vimtex_compiler_progname = "nvr" -- Neovim remote support
     vim.g.vimtex_compiler_latexmk = {
       build_dir = "build",

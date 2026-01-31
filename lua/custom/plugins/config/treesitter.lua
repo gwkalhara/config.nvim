f1e7dab26bda64887c66c42cec580cbc97b4bb34
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2026-01-31 11:18:37 +0530

    fix(treesitter): disable startup installation

--- lua/custom/plugins/config/treesitter.lua
+++ lua/custom/plugins/config/treesitter.lua
@@ -6,16 +6,21 @@ Treesitter.config = function(_, opts)
     "go",
     "lua",
 
+    "markdown",
+    "markdown_inline",
+    "html",
+    "latex",
+    "typst",
+    "yaml",
+
     "json",
     "toml",
 
     "gitcommit",
     "git_rebase",
-  },
-
-
+  }
   require("nvim-treesitter").setup(opts)
-  require("nvim-treesitter").install(parsers, {max_jobs=1})
+  -- require("nvim-treesitter").install(parsers, { max_jobs = 3 })
 
   -- vim.api.nvim_create_autocmd("FileType", {
   --   pattern = { "tex", "latex" },

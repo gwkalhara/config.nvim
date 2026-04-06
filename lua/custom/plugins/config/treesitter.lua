2e59efd3e0e231779f774a4facfede9c6ffeb270
gwkalhara <136357643+gwkalhara@users.noreply.github.com>
2026-04-06 19:00:07 +0530

    fix: treesitter

--- lua/custom/plugins/config/treesitter.lua
+++ lua/custom/plugins/config/treesitter.lua
@@ -1,24 +1,24 @@
-local parsers = {
-  "python",
-  "go",
-  "lua",
+-- local parsers = {
+--   "python",
+--   "go",
+--   "lua",
+--
+--   "markdown",
+--   "markdown_inline",
+--   "html",
+--   "latex",
+--   "typst",
+--   "yaml",
+--
+--   "json",
+--   "toml",
+--
+--   -- "gitcommit",
+--   "git_rebase",
+-- }
 
-  "markdown",
-  "markdown_inline",
-  "html",
-  "latex",
-  "typst",
-  "yaml",
-
-  "json",
-  "toml",
-
-  -- "gitcommit",
-  "git_rebase",
-}
-
-require("nvim-treesitter").setup(opts)
-require("nvim-treesitter").install(parsers, { max_jobs = 3 })
+require("nvim-treesitter").setup()
+-- require("nvim-treesitter").install(parsers, { max_jobs = 3 })
 
 -- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = { "tex", "latex" },
